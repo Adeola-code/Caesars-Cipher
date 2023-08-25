4f0902cf4560a25eef0830232e1584912ab1893e
Ojekunle Adeola <adeolaoluwa24@gmail.com>
2023-08-25 15:43:03 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import art
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    #The code keeps the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+#Import and print the logo from art.py when the program starts.
+logo=art.logo
+print(logo)
+
+#Ask the user if they want to restart the cipher program. 
+
+should_end=False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+#The program continues to work even if the user enters a shift number greater than 26. 
+
+    shift=shift%26
+    
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+#Ask the user whether to continue or not   
+    result=input("Do you want to restart? Type 'yes' if you want to go again. Otherwise type 'no'. ").lower()
+    if result=="no":
+        should_end=True
+        print("Goodbye")
